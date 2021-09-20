f3af588f3366731c3102740f38046da9c59e4919
Linny8675 <71115033+Linny8675@users.noreply.github.com>
2021-09-20 16:22:39 +1000

    35% Completion

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** References: 
+Audio: https://stackoverflow.com/questions/59439413/how-to-loop-an-audio-in-unity
+https://docs.unity3d.com/ScriptReference/AudioSource-clip.html
+**/
+
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip introBgm;
+    public AudioClip normalBgm;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+        audio.clip = normalBgm;
+        audio.loop = true;
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
