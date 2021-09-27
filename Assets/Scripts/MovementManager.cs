464af742f59cb0daf409c72f33bfb59cfce69b54
Linny8675 <71115033+Linny8675@users.noreply.github.com>
2021-09-27 17:02:09 +1000

    80% Completion

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    /**
+    private int lastTime = 0;
+    private float timer = 0;
+    **/
+    private Vector3 currentPos;
+    private Vector3 posOne;
+    //Animator pacStudent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPos = new Vector3(transform.position.x, transform.position.y);
+        posOne = new Vector3(-0.2f, transform.position.y);
+        // StartCoroutine(right());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /**
+        timer += Time.deltaTime;
+        if (lastTime == (int)timer)
+        {
+            lastTime++;
+        }
+        **/
+        transform.position = Vector3.Lerp(currentPos, posOne, Time.time);
+    }
+}
